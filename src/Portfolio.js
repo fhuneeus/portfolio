051eddd66c601d06aa3f58a3e424a34f56b9eca3
Federico Huneeus <fhuneeus@gmail.com>
2022-01-05 14:44:09 -0300

    fixing dates

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -12,7 +12,7 @@ export function Portfolio(){
     
     constructor(){
       let prices = [];
-      for(let i=0; i<365; i++){
+      for(let i=0; i<367; i++){
         prices.push(getRandomArbitrary(800,900));
       }
 
